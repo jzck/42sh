4c1338956a7845130b46e95b731477b92f65a06a
ariard@student.42.fr <ariard@student.42.fr>
2017-01-26 01:01:09 +0100

    lexing while_clause, passage en buffer dynamique au readscript todo tomorrow

---
+++ src/lexer/lexer_while.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_loop.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ariard <ariard@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/25 21:58:12 by ariard            #+#    #+#             */
+/*   Updated: 2017/01/26 00:58:57 by ariard           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+
+int		lexer_while(t_list **alst, char *str)
+{
+	t_token		*token;
+
+	token = (*alst)->content;
+	if (ft_strncmp(str, "while", 5) == 0)
+		token->type = TK_WHILE;
+	else if (ft_isalnum(*str))
+	{
+		token_append(token, *str);
+		return (ft_tokenize(alst, str + 1, WORD));
+	}
+	return (ft_tokenize(&(*alst)->next, str + 6, LIST));
+}
