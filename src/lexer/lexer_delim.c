7ed819dc8d02f13a145cfa0670e2a846943c79d9
Jack Halford <jack@crans.org>
2017-02-03 15:15:38 +0100

    lexer skips delims before starting to tokenize

--- src/lexer/lexer_delim.c
+++ src/lexer/lexer_delim.c
@@ -6,7 +6,7 @@
 /*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/03 11:58:44 by jhalford          #+#    #+#             */
-/*   Updated: 2017/01/26 16:30:43 by jhalford         ###   ########.fr       */
+/*   Updated: 2017/02/03 15:15:14 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		lexer_delim(t_list **alst, char *str)
 {
 	t_token		*token;
 
-	token = (*alst)->content;
+	token = *alst ? (*alst)->content : NULL;
 	while (ft_is_delim(*str))
 		str++;
 	if (token->type)
