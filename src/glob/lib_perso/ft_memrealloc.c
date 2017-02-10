74f8317b455bfb8e80503acdbd58336c96cb18cc
gwojda <gwojda@student.42.fr>
2017-02-10 12:19:51 +0100

    fixe termcaps

--- src/glob/lib_perso/ft_memrealloc.c
+++ src/glob/lib_perso/ft_memrealloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memrealloc.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wescande <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: wescande <wescande@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 13:44:36 by wescande          #+#    #+#             */
-/*   Updated: 2017/02/08 13:54:33 by wescande         ###   ########.fr       */
+/*   Updated: 2017/02/10 12:13:23 by gwojda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,6 @@ void	*ft_memrealloc(void *ptr, size_t old_s, size_t new_s)
 	}
 	n_ptr = ft_memalloc(new_s);
 	ft_memcpy(n_ptr, ptr, old_s);
-	ft_memdel(ptr);
+	ft_memdel(&ptr);
 	return (n_ptr);
 }
