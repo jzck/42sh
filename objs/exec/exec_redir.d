9ff30e63b7fb3a9bcc04eb82ca402694f677674e
ariard@student.42.fr <ariard@student.42.fr>
2017-02-21 14:36:32 +0100

    before pull

---
+++ objs/exec/exec_redir.d
@@ -0,0 +1,10 @@
+objs/exec/exec_redir.o: src/exec/exec_redir.c includes/exec.h \
+  libft/includes/libft.h libft/includes/lst.h libft/includes/dlst.h \
+  libft/includes/str.h libft/includes/sstr.h libft/includes/math.h \
+  libft/includes/btree.h libft/includes/mytime.h \
+  libft/includes/get_next_line.h libft/includes/color.h \
+  libft/includes/ft_sys.h includes/types.h includes/job_control.h \
+  includes/minishell.h includes/lexer.h includes/parser.h \
+  includes/ft_readline.h includes/ft_input.h includes/builtin.h \
+  includes/builtin_read.h includes/glob.h includes/completion.h \
+  includes/hash.h
