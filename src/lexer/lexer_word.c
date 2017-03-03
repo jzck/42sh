948a21702c4fca036ccb2a2d4c458d9075729255
wescande <wescande@student.42.fr>
2017-03-03 16:11:42 +0100

    modif assignment word

--- src/lexer/lexer_word.c
+++ src/lexer/lexer_word.c
@@ -6,12 +6,53 @@
 /*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/03 12:07:11 by jhalford          #+#    #+#             */
-/*   Updated: 2017/03/01 23:32:59 by ariard           ###   ########.fr       */
+/*   Updated: 2017/03/03 16:00:13 by wescande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
+static int	word_is_assignment(t_token *token)
+{
+	const int	len = ft_strlen(token->data);
+	int			pos = -1;
+	char		c;
+
+	while (++pos < len)
+	{
+		if (is_char_esc(token->esc, token->data, token->data + pos))
+			return (0);
+		c = token->data[pos];
+		if (!ft_isalnum(c) && c != '_' && c != '-')
+			return (0);
+	}
+	return (1);
+}
+
+int			lexer_word(t_list **alst, t_lexer *lexer)
+{
+	t_token		*token;
+	t_lexstate	state;
+
+	token = (*alst)->content;
+	token->type = TK_WORD;
+	if ((state = get_state_global(lexer))
+		|| (state = get_state_redir(lexer)))
+	{
+		lexer->state = state;
+		return (lexer_lex(alst, lexer));
+	}
+	if (lexer->str[lexer->pos] == '=' && word_is_assignment(token))
+	{
+		lexer->state = ASSIGNEMENT_WORD;
+		return (lexer_lex(alst, lexer));
+	}
+	token_append(token, lexer, 0, 0);
+	lexer->pos++;
+	return (lexer_lex(alst, lexer));
+}
+
+/*
 int		lexer_word(t_list **alst, t_lexer *lexer)
 {
 	t_token		*token;
@@ -33,3 +74,4 @@ int		lexer_word(t_list **alst, t_lexer *lexer)
 	lexer->pos++;
 	return (lexer_lex(alst, lexer));
 }
+*/
