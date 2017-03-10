611457299d2c0fdf2e7f7088038d19d286cab69b
Jack Halford <jack@crans.org>
2017-03-10 14:01:39 +0100

    redirection tweaks

--- src/exec/fd_is_valid.c
+++ src/exec/fd_is_valid.c
@@ -6,16 +6,18 @@
 /*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/03 13:46:40 by jhalford          #+#    #+#             */
-/*   Updated: 2017/03/06 16:58:38 by jhalford         ###   ########.fr       */
+/*   Updated: 2017/03/10 13:59:28 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		fd_is_valid(int fd, int has_flag)
+int		fd_is_valid(int fd, int has_mode)
 {
-	int		flags;
+	int		fd_flags;
+	int		fd_modes;
 
-	flags = fcntl(fd, F_GETFD);
-	return ((flags != -1 || errno != EBADF) && flags & has_flag);
+	fd_modes = fcntl(fd, F_GETFL);
+	fd_flags = fcntl(fd, F_GETFD);
+	return ((fd_flags != -1 || errno != EBADF) && fd_modes & has_mode);
 }
