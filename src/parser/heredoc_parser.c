2d261e89f7d635d7e897ba2a1399f9548525ed08
Antoine Riard <ariard@student.42.fr>
2017-03-11 18:50:38 +0100

    heredoc debug en cours

--- src/parser/heredoc_parser.c
+++ src/parser/heredoc_parser.c
@@ -6,7 +6,7 @@
 /*   By: ariard <ariard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/08 16:21:05 by ariard            #+#    #+#             */
-/*   Updated: 2017/03/11 16:20:21 by ariard           ###   ########.fr       */
+/*   Updated: 2017/03/11 18:50:14 by ariard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,17 @@ int			pop_heredoc(t_list **lst)
 				temp2 = temp->next;
 				free(temp);
 				data_singleton()->heredoc_queue = temp2;
+				DG("type is :%s", read_state(head->type));
+				DG("heredoc data after clear: %s", head->heredoc_data);
 			}
 			else
+			{
+				DG("type is :%s", read_state(head->type));
 				head->heredoc_data = ft_strjoin(head->heredoc_data,
 					token->data);
+				head->heredoc_data = ft_strjoin(head->heredoc_data, "\n");
+				DG("heredoc data + one: %s", head->heredoc_data);
+			}
 		}
 		ft_lstdel(lst, &token_free);
 		return (1);
