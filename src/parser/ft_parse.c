e1ec4a6d6093e38408584d1c4ae842c171ac41b5
Jack Halford <jack@crans.org>
2017-03-15 17:57:31 +0100

    close p.to_close before redirections

--- src/parser/ft_parse.c
+++ src/parser/ft_parse.c
@@ -6,7 +6,7 @@
 /*   By: ariard <ariard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/11 16:17:38 by ariard            #+#    #+#             */
-/*   Updated: 2017/03/13 17:42:06 by ariard           ###   ########.fr       */
+/*   Updated: 2017/03/15 17:56:56 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ int				ft_parse(t_btree **ast, t_list **token, t_parser *parser)
 		else
 			parser->state = UNDEFINED;
 		build_tree(ast, token);
-		btree_print(STDBUG, *ast, &ft_putast);
+		/* btree_print(STDBUG, *ast, &ft_putast); */
 		if ((end_instruction(&parser->stack) && !(*token)->next))
 			insert_linebreak(token);
 		else
