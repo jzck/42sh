735b045bf71bd0911a626c95e7ba9197bbb4a84c
wescande <wescande@student.42.fr>
2017-03-18 00:52:06 +0100

    rectif job display

--- src/job-control/job_notify_change.c
+++ src/job-control/job_notify_change.c
@@ -6,7 +6,7 @@
 /*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/12 15:04:03 by jhalford          #+#    #+#             */
-/*   Updated: 2017/03/17 22:51:40 by wescande         ###   ########.fr       */
+/*   Updated: 2017/03/18 00:51:40 by wescande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void	job_notify_change(int id)
 
 	jobc = &data_singleton()->jobc;
 	job = ft_lst_find(jobc->first_job, &id, job_cmp_id)->content;
-	if (job->attrs & JOB_BG)
+//	if (job->attrs & JOB_BG) utilise pour notif dynamic
 		job_format(job, JOBS_OPTS_L);
 }
