9d5dcf0091341ad87a84316c5039245121a55b71
wescande <wescande@student.42.fr>
2017-03-18 01:58:00 +0100

    rectif retour des while et until

--- src/exec/plaunch_until.c
+++ src/exec/plaunch_until.c
@@ -6,7 +6,7 @@
 /*   By: wescande <wescande@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/07 22:04:42 by wescande          #+#    #+#             */
-/*   Updated: 2017/03/17 20:20:04 by wescande         ###   ########.fr       */
+/*   Updated: 2017/03/18 01:52:33 by wescande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int				plaunch_until(t_process *p)
 {
 	int		ret;
 
+	ret = 0;
 	ft_exec(&p->data.d_until.condition);
-	ret = ft_atoi(ft_getenv(data_singleton()->env, "?"));
 	while (ft_strcmp(ft_getenv(data_singleton()->env, "?"), "0"))
 	{
 		ft_exec(&p->data.d_until.content);
--- src/exec/plaunch_while.c
+++ src/exec/plaunch_while.c
@@ -6,7 +6,7 @@
 /*   By: wescande <wescande@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/07 17:20:53 by wescande          #+#    #+#             */
-/*   Updated: 2017/03/17 20:16:10 by wescande         ###   ########.fr       */
+/*   Updated: 2017/03/18 01:48:53 by wescande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int				plaunch_while(t_process *p)
 {
 	int		ret;
 
+	ret = 0;
 	ft_exec(&p->data.d_while.condition);
-	ret = ft_atoi(ft_getenv(data_singleton()->env, "?"));
 	while (!(ft_strcmp(ft_getenv(data_singleton()->env, "?"), "0")))
 	{
 		ft_exec(&p->data.d_while.content);
