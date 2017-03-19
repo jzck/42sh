ef86f2212d747d4e18eb07012cd523dbacb838c6
wescande <wescande@student.42.fr>
2017-03-19 19:37:10 +0100

    nothing

--- src/exec/process_resetfds.c
+++ src/exec/process_resetfds.c
@@ -6,7 +6,7 @@
 /*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/09 14:51:23 by jhalford          #+#    #+#             */
-/*   Updated: 2017/03/18 04:13:04 by wescande         ###   ########.fr       */
+/*   Updated: 2017/03/19 19:36:11 by wescande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,12 @@ void	process_resetfds(t_process *p)
 	i = 0;
 	while (i < 10)
 	{
-		//if (i!=3) //JACK SOME PB HERE  on close la sortie debug en forcant le close sur tous les fd... ne risque-t-on pas autre chose ??!!!
+		/* if (i!=3) //JACK SOME PB HERE  on close la sortie debug en forcant le close sur tous les fd... ne risque-t-on pas autre chose ??!!! */
+		/* { */
 			close(i++);
+		/* } */
+		/* else */
+		/* 	i++; */
 	}
 	dup2(exec->fd_save[0], STDIN);
 	dup2(exec->fd_save[1], STDOUT);
