352f8e0b5813f14ca5999851dea6ebf27bf6a504
Jack Halford <jack@crans.org>
2017-03-21 16:08:26 +0100

    added ft_malloc

---
+++ libft/src/mem/ft_malloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_malloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/21 15:45:00 by jhalford          #+#    #+#             */
+/*   Updated: 2017/03/21 15:45:20 by jhalford         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_malloc(size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(size);
+	if (!ptr)
+	{
+		ft_dprintf(2, "{red}%s{eoc}", strerror(errno));
+		exit(errno);
+	}
+	return (ptr);
+}
