77096362f03562e1ae52e9362e385cbb532231f5
Jack Halford <jack@crans.org>
2017-03-22 18:14:26 +0100

    curly brackets lexing

--- src/job_control/sigint_handler.c
+++ src/job_control/sigint_handler.c
@@ -6,7 +6,7 @@
 /*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/10 15:14:47 by jhalford          #+#    #+#             */
-/*   Updated: 2017/01/22 20:56:45 by ariard           ###   ########.fr       */
+/*   Updated: 2017/03/22 18:04:22 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 void	sigint_handler(int signo)
 {
 	(void)signo;
+	set_exitstatus(1, 1);
 	DG("pid:%i got SIGINT", getpid());
 }
