5394a774e136bdc82186c952c1236c8a386bcf98
Jack Halford <jack@crans.org>
2017-03-22 17:17:08 +0100

    issue #144

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: gwojda <gwojda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/20 14:45:40 by gwojda            #+#    #+#             */
-/*   Updated: 2017/03/22 15:54:01 by jhalford         ###   ########.fr       */
+/*   Updated: 2017/03/22 17:13:06 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,8 @@ static int		handle_instruction(t_list **token, t_btree **ast)
 			return (ret);
 		if (do_lexer_routine(token, stream) > 0)
 			continue ;
-		/* token_print(*token); */
+		token_print(*token);
+		exit(1);
 		if ((ret = do_parser_routine(token, ast)) == 1
 			&& SH_NO_INTERACTIVE(data->opts))
 			return (ret);
