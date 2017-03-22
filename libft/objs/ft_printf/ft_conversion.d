2574c38426e6246d89316e710af729625c946654
gwojda <gwojda@student.42.fr>
2017-03-22 15:33:10 +0100

    les binaires c'est tabou, on en viendra tous a bout

--- libft/objs/ft_printf/ft_conversion.d
+++
@@ -1,5 +0,0 @@
-objs/ft_printf/ft_conversion.o: src/ft_printf/ft_conversion.c \
-  includes/ft_printf.h includes/libft.h includes/error.h \
-  includes/color.h includes/cliopts.h includes/lst.h includes/dlst.h \
-  includes/btree.h includes/str.h includes/sstr.h includes/math.h \
-  includes/mytime.h includes/get_next_line.h includes/sys.h
--- libft/objs/math/ft_ulltoa_base.d
+++
@@ -1,5 +0,0 @@
-objs/math/ft_ulltoa_base.o: src/math/ft_ulltoa_base.c includes/libft.h \
-  includes/error.h includes/color.h includes/cliopts.h includes/lst.h \
-  includes/dlst.h includes/btree.h includes/str.h includes/sstr.h \
-  includes/math.h includes/mytime.h includes/get_next_line.h \
-  includes/sys.h
