9112636725faabeaccaf98a5ee793d44e6526a05
Jack Halford <jack@crans.org>
2017-03-23 13:30:23 +0100

    issue #162

--- src/main/data_exit.c
+++ src/main/data_exit.c
@@ -6,7 +6,7 @@
 /*   By: jhalford <jhalford@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/07 18:07:50 by jhalford          #+#    #+#             */
-/*   Updated: 2017/03/21 14:24:52 by jhalford         ###   ########.fr       */
+/*   Updated: 2017/03/23 13:29:40 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	data_exit(void)
 	ft_sstrfree(data->env);
 	ft_sstrfree(data->local_var);
 	ft_sstrfree(data->argv);
+	lexer_destroy(&data->lexer);
+	parser_destroy(&data->parser);
 	ft_save_termios(-1);
 	ft_free_hash_table();
 	free_history_list(data_singleton()->line.list_beg);
