6cf25986557e9cdcd0bd47e01ea7a4261eb5f3be
Antoine Riard <ariard@student.42.fr>
2017-03-24 19:17:40 +0100

    oubli while_clause and tk_paren_open close #188

--- src/lexer/token_print.c
+++ src/lexer/token_print.c
@@ -6,7 +6,7 @@
 /*   By: jhalford <jhalford@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 14:39:01 by jhalford          #+#    #+#             */
-/*   Updated: 2017/03/24 17:24:09 by gwojda           ###   ########.fr       */
+/*   Updated: 2017/03/24 18:59:40 by ariard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	token_print(t_list *lst)
 	{
 		if (lst->content)
 			token = lst->content;
+		DG("%s", read_state(token->type));
 		lst = lst->next;
 	}
 }
