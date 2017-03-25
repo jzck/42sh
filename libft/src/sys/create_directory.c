b8e9abae1848f847b6b58be78c07e81cd0b9f75f
Antoine Riard <ariard@student.42.fr>
2017-03-25 04:13:45 +0100

    create dir in libft

--- libft/src/sys/create_directory.c
+++ libft/src/sys/create_directory.c
@@ -6,7 +6,7 @@
 /*   By: ariard <ariard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/25 03:18:18 by ariard            #+#    #+#             */
-/*   Updated: 2017/03/25 04:07:10 by ariard           ###   ########.fr       */
+/*   Updated: 2017/03/25 04:13:15 by ariard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,21 @@ char		*create_directory(const char *path, const char *old_pathnames)
 {
 	char	*new_pathnames;
 	char	*newdir;
+	char	*temp;
 	char	*semi;
 
 	new_pathnames = ft_strdup(old_pathnames);
 	temp = new_pathnames;
 	while (new_pathnames)
 	{
-		if ((semi = ft_strchr(new_pathnames, ":")))
+		if ((semi = ft_strchr(new_pathnames, ':')))
 			*semi = 0;
 		if (!is_directory(newdir = ft_str3join(new_pathnames, "/", path)))
 			ft_strdel(&newdir);
 		else
 			break;
-		new_pathnames += new_pathnames + 1;
+		new_pathnames += ft_strlen(new_pathnames) + 1;
 	}
-	ft_strdel(temp);
+	ft_strdel(&temp);
 	return (newdir);
 }
