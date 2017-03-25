bc9faa58b8a966976a6b40671ebec9572b2b775f
Jack Halford <jack@crans.org>
2017-03-25 02:36:44 +0100

    open_access fixes

--- libft/src/sys/open_access.c
+++ libft/src/sys/open_access.c
@@ -6,7 +6,7 @@
 /*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/25 01:10:56 by jhalford          #+#    #+#             */
-/*   Updated: 2017/03/25 02:19:58 by jhalford         ###   ########.fr       */
+/*   Updated: 2017/03/25 02:26:43 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int		open_access(char *file, t_flag a_flag, t_flag o_flag, t_flag o_perm)
 {
 	int		fd;
 
-	if (access(file, F_OK) != 0)	
+	if (a_flag & F_OK && access(file, F_OK) != 0)	
 		return (-ERR_SET(E_SYS_NOFILE, file));
 	if (is_directory(file))
 		return (-ERR_SET(E_SYS_ISDIR, file));
-	if (access(file, a_flag) != 0)
+	if (access(file, F_OK) == 0 && access(file, a_flag) != 0)
 		return (-ERR_SET(E_SYS_NOPERM, file));
 	if ((fd = open(file, o_flag, o_perm)) < 0)
 	{
--- src/exec/redirect_less.c
+++ src/exec/redirect_less.c
@@ -6,7 +6,7 @@
 /*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/06 22:09:53 by jhalford          #+#    #+#             */
-/*   Updated: 2017/03/25 01:52:33 by jhalford         ###   ########.fr       */
+/*   Updated: 2017/03/25 02:25:58 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		redirect_less(t_redir *redir)
 	int		fdnew;
 
 	fdnew = redir->n;
-	if ((fdold = open_access(redir->word, W_OK,
+	if ((fdold = open_access(redir->word, F_OK | W_OK,
 					O_RDONLY, 0)) != 0)
 		return (ft_perror(NULL));
 	dup2(fdold, fdnew);
