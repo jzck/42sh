78014e892bcab5ce6acda7a15b159cc8c36690c8
Antoine Riard <ariard@student.42.fr>
2017-03-25 02:05:06 +0100

    pull cd jack

--- libft/objs/btree/btree_del.d
+++
@@ -1,5 +0,0 @@
-objs/btree/btree_del.o: src/btree/btree_del.c includes/btree.h \
-  includes/libft.h includes/error.h includes/color.h includes/cliopts.h \
-  includes/lst.h includes/dlst.h includes/str.h includes/sstr.h \
-  includes/math.h includes/mytime.h includes/get_next_line.h \
-  includes/sys.h
