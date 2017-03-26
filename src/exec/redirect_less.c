a35f321f31dce75e6bcecae1f17ed8be1bd24dd9
Jack Halford <jack@crans.org>
2017-03-26 18:26:08 +0200

    redirect less error msg close #199

--- src/exec/redirect_less.c
+++ src/exec/redirect_less.c
@@ -6,7 +6,7 @@
 /*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/06 22:09:53 by jhalford          #+#    #+#             */
-/*   Updated: 2017/03/25 02:25:58 by jhalford         ###   ########.fr       */
+/*   Updated: 2017/03/26 18:25:38 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		redirect_less(t_redir *redir)
 
 	fdnew = redir->n;
 	if ((fdold = open_access(redir->word, F_OK | W_OK,
-					O_RDONLY, 0)) != 0)
+					O_RDONLY, 0)) < 0)
 		return (ft_perror(NULL));
 	dup2(fdold, fdnew);
 	return (0);
