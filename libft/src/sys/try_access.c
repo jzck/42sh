bb937f5f393169d4476b1ce1941cede8674ca415
Jack Halford <jack@crans.org>
2017-03-27 16:59:20 +0200

    :umbrella: c'est bueno les enfants

--- libft/src/sys/try_access.c
+++ libft/src/sys/try_access.c
@@ -6,7 +6,7 @@
 /*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/25 01:10:56 by jhalford          #+#    #+#             */
-/*   Updated: 2017/03/26 22:36:13 by jhalford         ###   ########.fr       */
+/*   Updated: 2017/03/27 16:58:26 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 int		try_access(char *file, int exists, t_flag a_flag)
 {
 	if (exists && access(file, F_OK) != 0)
-		return (ERR_SET(E_SYS_NOFILE, file) * 0 - 1);
+		return (ERR_SET(E_SYS_NOFILE, file));
 	else if (is_directory(file))
-		return (ERR_SET(E_SYS_ISDIR, file) * 0 - 1);
+		return (ERR_SET(E_SYS_ISDIR, file));
 	else if (access(file, F_OK) == 0 && access(file, a_flag) != 0)
-		return (ERR_SET(E_SYS_NOPERM, file) * 0 - 1);
+		return (ERR_SET(E_SYS_NOPERM, file));
 	return (0);
 }
