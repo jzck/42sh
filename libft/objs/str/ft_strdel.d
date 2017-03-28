c73a7bb8c4970dab3c4e6ddfee5181db58a9661b
Antoine Riard <ariard@student.42.fr>
2017-03-28 23:25:23 +0200

    heredoc to test

--- libft/objs/str/ft_strdel.d
+++
@@ -1,5 +0,0 @@
-objs/str/ft_strdel.o: src/str/ft_strdel.c includes/libft.h \
-  includes/ft_types.h includes/error.h includes/color.h \
-  includes/cliopts.h includes/lst.h includes/dlst.h includes/btree.h \
-  includes/str.h includes/sstr.h includes/math.h includes/mytime.h \
-  includes/get_next_line.h includes/sys.h
